5aeb67db3347f44f6bc1c172dfae57c17b685c14
Christoffer Zakrisson <rustypig91@gmail.com>
2025-06-01 15:07:23 +0200

    oops

--- src/gameplay/movementcontrols.rs
+++ src/gameplay/movementcontrols.rs
@@ -1,1 +1,8 @@
 use super::*;
+use bevy_enhanced_input::prelude::*;
+
+#[derive(Debug, InputAction)]
+#[input_action(output = Vec2)]
+struct Move;
+
+fn tractor_move(mut tractor: Single<&mut Transform, &Tractor>, time: Res<Time>) {}
