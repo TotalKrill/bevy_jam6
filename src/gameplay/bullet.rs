2bf9bc46cc749f7de2d27392bc2c74ee5a15b35f
Kristoffer Ödmark <kristoffer.odmark90@gmail.com>
2025-06-05 14:39:22 +0200

    wip

--- src/gameplay/bullet.rs
+++ src/gameplay/bullet.rs
@@ -1,12 +1,8 @@
-use std::collections::HashSet;
 use std::time::Duration;
 
-use bevy::color::palettes::css::*;
-use bevy::ecs::observer::TriggerTargets;
+use super::*;
 use crate::gameplay::apple::Apple;
 use crate::gameplay::health::Damage;
-use crate::gameplay::tractor::{LeftWheels, RightWheels, Tractor};
-use super::*;
 
 pub fn bullet_plugin(app: &mut App) {
     app.add_systems(Update, (despawn_bullets, shoot_apples));
@@ -18,15 +14,18 @@ fn despawn_bullets(
     mut bullets: Query<(Entity, &mut Bullet)>,
 ) {
     for (e, mut bullet) in bullets.iter_mut() {
-        bullet.0.tick(time.delta());
-        if bullet.0.just_finished() {
+        bullet.timer.tick(time.delta());
+        if bullet.timer.just_finished() {
             commands.entity(e).despawn();
         }
     }
 }
 
 #[derive(Component)]
-pub struct Bullet(pub Timer);
+pub struct Bullet {
+    pub timer: Timer,
+    pub damage: f32,
+}
 
 pub fn bullet(
     meshes: &mut Assets<Mesh>,
@@ -44,7 +43,10 @@ pub fn bullet(
         Collider::sphere(0.1),
         LinearVelocity(direction * speed),
         Transform::from_rotation(Quat::from_rotation_x(90f32.to_radians())).with_translation(at), // .rotate_local_x(90f32.to_radians()),
-        Bullet(Timer::new(Duration::from_secs(3), TimerMode::Once)),
+        Bullet {
+            timer: Timer::new(Duration::from_secs(3), TimerMode::Once),
+            damage: 100.,
+        },
         PointLight {
             color: ORANGE_RED.into(),
             intensity: 10_000.,
@@ -61,16 +63,18 @@ fn shoot_apples(
     apples: Query<Entity, With<Apple>>,
     mut event_writer: EventWriter<Damage>,
 ) {
-
     for CollisionStarted(entity1, entity2) in collision_event_reader.read() {
-
-        if let (Ok(apple), Ok(bullet)) = (apples.get(*entity1), apples.get(*entity1)) {
-            if let Ok(bullet) = bullets.get(*entity2) {
-                event_writer.write(Damage {
-                    value: 100.0,
-                    entity: apple,
-                });
-            }
+        if let (Ok(apple), Ok(bullet)) = (apples.get_many(*entity1), apples.get_many(*entity2)) {
+            event_writer.write(Damage {
+                value: 100.0,
+                entity: apple,
+            });
+        }
+        if let (Ok(apple), Ok(bullet)) = (apples.get(*entity2), apples.get(*entity1)) {
+            event_writer.write(Damage {
+                value: 100.0,
+                entity: apple,
+            });
         }
     }
 }
