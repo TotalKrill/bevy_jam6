dc48a5f39b9d0f837a30d9deb04dc79849740d74
philip435 <philip.lindb@gmail.com>
2025-06-07 15:59:31 +0200

    added turrent animation

--- src/gameplay/turret.rs
+++ src/gameplay/turret.rs
@@ -1,5 +1,6 @@
 use std::time::Duration;
-
+use bevy_tweening::{Animator, RepeatStrategy, Tween};
+use bevy_tweening::lens::TransformPositionLens;
 use super::*;
 
 pub const BARREL_LEN: f32 = 2.0;
@@ -27,18 +28,32 @@ impl Default for TurretDamage {
 }
 
 use crate::gameplay::bullet::BulletSpawnEvent;
+use crate::gameplay::tree::Tree;
 
 #[cfg_attr(feature = "dev_native", hot)]
 fn tick_and_fire_turret(
+    mut commands: Commands,
     time: Res<Time>,
-    mut turrets: Query<(&mut Turret, &GlobalTransform, &TurretDamage)>,
+    mut turrets: Query<(Entity, &Transform, &mut Turret, &GlobalTransform, &TurretDamage)>,
     mut fire_bullet_evt: EventWriter<BulletSpawnEvent>,
 ) {
     use crate::gameplay::bullet::Bullet;
 
-    for (mut turret, transform, turret_damage) in turrets.iter_mut() {
+    for (entity, local_transform, mut turret, transform, turret_damage) in turrets.iter_mut() {
         turret.rate_of_fire.tick(time.delta());
         if turret.rate_of_fire.finished() && turret.firing {
+
+            commands
+                .entity(entity)
+                .insert(Animator::new(Tween::new(
+                    EaseFunction::Linear,
+                    Duration::from_millis(90), // TODO Scale with damage/speed ?
+                    TransformPositionLens {
+                        start: local_transform.translation,
+                        end: local_transform.translation - local_transform.forward().normalize() * 0.45, // TODO Scale with damage/speed ?
+                    },
+                ).with_repeat_count(2).with_repeat_strategy(RepeatStrategy::MirroredRepeat)));
+
             turret.rate_of_fire.reset();
             let forward = transform.forward();
             let bullet_spawnpoint = transform.translation() + (BARREL_LEN + 0.5) * forward;
