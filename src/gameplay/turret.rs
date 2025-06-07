91604256d841d02e0488112b176b37ebc8815f64
philip435 <philip.lindb@gmail.com>
2025-06-07 16:24:25 +0200

    use get_entity

--- src/gameplay/turret.rs
+++ src/gameplay/turret.rs
@@ -43,9 +43,8 @@ fn tick_and_fire_turret(
         turret.rate_of_fire.tick(time.delta());
         if turret.rate_of_fire.finished() && turret.firing {
 
-            commands
-                .entity(entity)
-                .insert(Animator::new(Tween::new(
+            if let Ok(mut entity) = commands.get_entity(entity) {
+                entity.insert(Animator::new(Tween::new(
                     EaseFunction::Linear,
                     Duration::from_millis(90), // TODO Scale with damage/speed ?
                     TransformPositionLens {
@@ -53,7 +52,8 @@ fn tick_and_fire_turret(
                         end: local_transform.translation - local_transform.forward().normalize() * 0.45, // TODO Scale with damage/speed ?
                     },
                 ).with_repeat_count(2).with_repeat_strategy(RepeatStrategy::MirroredRepeat)));
-
+            }
+            
             turret.rate_of_fire.reset();
             let forward = transform.forward();
             let bullet_spawnpoint = transform.translation() + (BARREL_LEN + 0.5) * forward;
